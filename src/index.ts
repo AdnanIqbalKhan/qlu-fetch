d79f764d71eec4b31b5a9c2b03d3e14cb3f424dd
AdnanIqbalKhan <hafizadnaniqbalkhan@gmail.com>
2023-02-20 12:41:44 +0500

    add delay

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,27 @@
 import fetch from 'node-fetch'
 
-export const QluFetch = async function (url: string, options = {}, retries = 3): Promise<any> {
+
+export const QluFetch = async function (url: string, options = {}, retries = 3, delay: Array<number> | number = 1): Promise<any> {
     return fetch(url, options)
         .then(res => {
             if (res.ok) {
                 return res.json()
             }
-            if (retries > 0) {
-                return QluFetch(url, options, retries - 1)
-            }
             throw new Error(`ERROR: ${res.status}`)
         })
         .catch(error => {
             if (retries > 0) {
-                return QluFetch(url, options, retries - 1)
+                return new Promise((resolve) => {
+                    const sleep = Array.isArray(delay)
+                        ? delay.length > 0
+                            ? delay.shift()
+                            : 1
+                        : delay
+
+                    setTimeout(() => {
+                        resolve(QluFetch(url, options, retries - 1));
+                    }, sleep);
+                });
             }
             console.error(error.message)
         })
